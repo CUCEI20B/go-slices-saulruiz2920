8183e3e47729a751fb27b1d0a6252cfe37feaf2b
SAUL FRANCISCO FRANCISCO <saul.ruiz2920@alumnos.udg.mx>
2020-10-14 18:49:59 +0000

    Sumation of a slice

--- main.go
+++ main.go
@@ -3,5 +3,17 @@ package main
 import "fmt"
 
 func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+  var n int
+  fmt.Scan(&n)
+  var slice []int
+  for i := 0; i < n ; i++ {
+    var input int
+    fmt.Scan(&input)
+    slice = append(slice, input)
+  }
+  var res int
+  for i := 0; i < len(slice); i++ {
+    res += slice[i]
+  }
+  fmt.Print(res)
 }
